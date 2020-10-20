5a9f885de8f1108ea2a532999462794de471b02c
GLOBOGAMES\t.lemeshko <t.lemeshko@globogames.ru>
2020-10-20 14:02:47 +0300

    sln

---
+++ CrvService/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CrvService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return new HealthResult
+            {
+                Code = (int)HealthResult.HealthStatus.Ok,
+                Status = "OK"
+            };
+        }
+    }
+}
